2e1596f500437ec2de2fad854787dcd3cae4f741
prajwal <heighlights57@gmail.com>
2024-05-27 20:00:31 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://127.0.0.1:2707/foodonline' // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+           if (err) console.log("---" + err)
+        else {
+                      console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("Categories");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+                  }
+    })
+};
